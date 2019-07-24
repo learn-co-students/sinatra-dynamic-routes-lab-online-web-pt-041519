6cc4df03bd11a710a3e5cb9d520cf35f94e0b145
Michael Chen <michaelchengaming@gmail.com>
2019-07-24 16:49:01 -0700

    Done.

--- app.rb
+++ app.rb
@@ -3,38 +3,36 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   # Write your code here!
   get '/reversename/:name' do
-    @reversed_name = params[:name].reverse
-    "#{@reversed_name}"
+    params[:name].reverse
   end
 
   get '/square/:number' do
-    @squared = params[:number].to_i ** 2
-    "#{@squared}"
+    squared = params[:number].to_i ** 2
+    squared.to_s
   end
 
   get '/say/:number/:phrase' do
-    @returned = params[:phrase] * params[:number].to_i
-    "#{@returned}"
+    params[:phrase] * params[:number].to_i
   end
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
-    @string = "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
+    "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
   end
 
   get '/:operation/:number1/:number2' do
-    @operation = params[:operation]
-    @num1 = params[:number1].to_i
-    @num2 = params[:number2].to_i
+    operation = params[:operation]
+    num1 = params[:number1].to_i
+    num2 = params[:number2].to_i
 
-    case @operation
+    case operation
     when "add"
-      answer = @num1 + @num2
+      answer = num1 + num2
     when "subtract"
-      answer = @num1 - @num2
+      answer = num1 - num2
     when "multiply"
-      answer = @num1 * @num2
+      answer = num1 * num2
     when "divide"
-      answer = @num1 / @num2
+      answer = num1 / num2
     end
     answer.to_s
   end
